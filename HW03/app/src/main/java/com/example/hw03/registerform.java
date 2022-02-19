d87fabd9fb0f7a8edd8285feea191fb248467146
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-02-19 16:17:50 +0700

    reset done

--- HW03/app/src/main/java/com/example/hw03/registerform.java
+++ HW03/app/src/main/java/com/example/hw03/registerform.java
@@ -6,7 +6,9 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -15,6 +17,8 @@ public class registerform extends AppCompatActivity {
 
     TextView username,password,retype,dob;
     Button reset,datePicker;
+    RadioGroup gender;
+    CheckBox tennis,futbal,others;
     String dob_data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +28,24 @@ public class registerform extends AppCompatActivity {
         password= findViewById(R.id.passwordInput);
         retype= findViewById(R.id.retypeInput);
         reset=findViewById(R.id.resetBtn);
+        gender=findViewById(R.id.genderCheckers);
+        tennis=findViewById(R.id.checkbox_Tennis);
+        futbal=findViewById(R.id.checkbox_Futbal);
+        others=findViewById(R.id.checkbox_Others);
         dob=findViewById(R.id.dobInput);
         datePicker=findViewById(R.id.dobBtn);
+        //Click Reset Button
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 username.setText("");
                 password.setText("");
                 retype.setText("");
+                dob.setText("");
+                gender.clearCheck();
+                tennis.setChecked(false);
+                futbal.setChecked(false);
+                others.setChecked(false);
             }
         });
         datePicker.setOnClickListener(new View.OnClickListener() {
