41d77f8757062deffb7f46c09053b5f52f29bd33
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-02-19 11:14:07 +0700

    datepicker

--- HW03/app/src/main/java/com/example/hw03/MainActivity.java
+++ HW03/app/src/main/java/com/example/hw03/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.hw03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView username,password,retype;
-    Button reset;
+    Button reset,datePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         password= findViewById(R.id.passwordInput);
         retype= findViewById(R.id.retypeInput);
         reset=findViewById(R.id.resetBtn);
+        datePicker=findViewById(R.id.dobBtn);
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
                 retype.setText("");
             }
         });
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog= new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+                            {
+
+                            }
+                        }, 0, 0, 0);
+                datePickerDialog.show();
+            }
+
+        });
     }
+
 }
